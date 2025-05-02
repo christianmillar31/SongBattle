2a88a1661f37dcdc51dcf89efd30fc2fbf73d342
nerosighted <94204807+nerosighted@users.noreply.github.com>
2025-05-01 23:16:20 -0700

    Update project settings and remove app icon requirement

--- New Group/SongBattle/Configuration.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-enum Configuration {
-    static let bundleId = "com.example.SongBattle"
-    static let spotifyClientId = "c4788e07ffa548f78f8101af9c8aa0c5"
-    static let spotifyRedirectURI = "songbattle://spotify-callback"
-    
-    // Add any other configuration values here
-} 
--- Sources/SongBattle/Configuration.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import SwiftUI
- 
-public enum Configuration {
-    @AppStorage("spotifyClientId") public static var spotifyClientId: String = ""
-    public static let spotifyRedirectURI = "songbattle://spotify-callback"
-} 
